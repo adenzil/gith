1e5672220ce60796d5af3e8ea2c2f2bb4ff3bc28
Alister <alistercabral@gmail.com>
2015-12-28 14:22:04 +0530

    added angular js ...trimmed functions

---
+++ application/views/home.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+error_reporting(0);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Angular</title>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+  <script type="text/javascript" src="/js/index.js"></script>
+  <link rel="stylesheet" type="text/css" href="/css/index.css"></link>
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.6/animate.min.css">
+</head>
+<body>
+
+
+
+<div class="container-fluid outerdiv" ng-app="myApp" ng-controller="MovieController">
+
+
+
+  <noscript>
+    <div class="nojs">Javascript is either disabled or not supported in your browser. Please enable it or use a Javascript enabled browser.</div>
+  </noscript>
+
+  <div class="animated zoomInRight">
+
+    <div class="input-group search-bar">
+      <input type="text" ng-model="search" ng-change="change()" ng-keyup="enter($event)" onclick="select()" class="form-control" placeholder="Enter github username" autofocus />
+      <span class="input-group-addon bar-style"><i class="glyphicon glyphicon-search"></i></span>
+    </div>
+    
+  </div>
+
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('myApp', [])
+  .controller('MovieController', function($scope, $http) {
+    var pendingTask;
+
+  });
